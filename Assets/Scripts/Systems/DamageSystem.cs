b13fb7d3f84df19526d6f33fa54bb5ed46733ce1
Adamujj <71527211+Xerszesz@users.noreply.github.com>
2025-08-14 10:05:02 +0200

    Különböző seződés effekt a Hősnek és Ellenségnek
    A hős piros sebzést kap, az ellenség kéket

--- Assets/Scripts/Systems/DamageSystem.cs
+++ Assets/Scripts/Systems/DamageSystem.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class DamageSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject damageVFX;
+    [SerializeField] private GameObject enemyDamagedVFX;
+    [SerializeField] private GameObject heroDamagedVFX;
 
     private void OnEnable()
     {
@@ -20,19 +21,27 @@ public class DamageSystem : MonoBehaviour
         foreach (var target in dealDamageGA.Targets)
         {
             target.Damage(dealDamageGA.Amount);
-            Instantiate(damageVFX, target.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(0.15f);
+            if (target is EnemyView enemyView)
+            {
+                Instantiate(enemyDamagedVFX, target.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(heroDamagedVFX, target.transform.position, Quaternion.identity);
+            }
+
+                yield return new WaitForSeconds(0.15f);
             if (target.CurrentHealth <= 0)
             {
-                if (target is EnemyView enemyView)
+                if (target is EnemyView enemyView1)
                 {
-                    KillEnemyGameAction killEnemyGA = new(enemyView);
+                    KillEnemyGameAction killEnemyGA = new(enemyView1);
                     ActionSystem.Instance.AddReaction(killEnemyGA);
                 }
                 else
                 {
                     // Nothing for now
-                    //Game Over Scene
+                    //Game Over Scene, Hero lost all health
                 }
             }
         }
